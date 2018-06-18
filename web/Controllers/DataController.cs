aee622fb054ba57887ce170dacfa16a2426503f1
kennebel <jac@kennebel.com>
2018-06-18 16:14:15 -0600

    Setting up a rough data file.

---
+++ web/Controllers/DataController.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace web.Controllers
+{
+    [Route("api/[controller]")]
+    public class DataController : Controller
+    {
+        static private InfoDb IDB { get; set; }
+        private IConfiguration config { get; set; }
+
+        public DataController(IConfiguration configuration)
+        {
+            config = configuration;
+        }
+
+        [HttpGet("[action]")]
+        public IEnumerable<SpaceEvent> AllSpaceEvents()
+        {
+            if (IDB == null || IDB.Data == null)
+            {
+                ReadData();
+            }
+
+            return IDB != null ? IDB.Data : null;
+        }
+
+        protected void ReadData()
+        {
+            IDB = new InfoDb();
+
+            try
+            {
+                string DataContent = System.IO.File.ReadAllText(config.GetSection("Data").GetSection("Path").Value);
+                IDB = Newtonsoft.Json.JsonConvert.DeserializeObject<InfoDb>(DataContent);
+            }
+            catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+        }
+
+        public class InfoDb
+        {
+            public Dictionary<string, string> Metadata { get; set; }
+            public List<SpaceEvent> Data { get; set; }
+        }
+
+        public class SpaceEvent
+        {
+            public string Name { get; set; }
+            public string Country { get; set; }
+            public DateTime Date { get; set; }
+            public string URL { get; set; }
+
+            public int Year { get { return Date.Year; } }
+            public int Month { get { return Date.Month; } }
+            public int Day { get { return Date.Day; } }
+            public string GenericDate { get { return Date.ToString("MM-dd"); } }
+        }
+    }
+}
